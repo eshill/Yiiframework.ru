c0f56ebf403b1b719075324bb5e9aa312c7f705a
Sergei Khilkov <admin@MacBook-Air.local>
2015-11-06 22:35:43 +0300

    Inside page layout start
    not ready

--- Html/js/yiiscript.js
+++ Html/js/yiiscript.js
@@ -1,5 +1,13 @@
 $("document").ready(function(){
 	$(".ico-f-close").click(function(){
-		$(".yii-features").toggleClass('f-hidden');
+		$(".yii-features").toggleClass('f-hidden'); // Close features
 	});
+
+	$(".subnav-btn").click(function(){
+		$(".subnav-all").toggleClass('subnav-visible'); // Show subnav on mobile
+	});
+
+	$(".sect-title").sticky({ topSpacing: 0, bottomSpacing: 840, center:true, className:"title-stick" }); // Sticker page title
+	$(".sect-subtitle").sticky({ topSpacing: 48, bottomSpacing: 800, center:true, className:"subtitle-stick" }); // Sticker page subtitle
+	$(".subnav").sticky({ topSpacing: 140, bottomSpacing: 400, center:true, className:"subnav-stick" }); // Sticker page subnav
 });
