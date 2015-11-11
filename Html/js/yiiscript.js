10ffd8441f0bec94e265d81171f4d6609a68e4c4
Sergei Khilkov <eshill@iMac-Sergej.local>
2015-11-11 16:27:23 +0300

    Questions selection added

--- Html/js/yiiscript.js
+++ Html/js/yiiscript.js
@@ -7,6 +7,18 @@ $("document").ready(function(){
 		$(".subnav-all").toggleClass('subnav-visible'); // Show subnav on mobile
 	});
 
+	$("#add-comm-01").click(function(){
+		$("#comm-01").toggleClass('comm-visible'); // Show add comment block
+	});
+
+	$("#add-comm-02").click(function(){
+		$("#comm-02").toggleClass('comm-visible'); // Show add comment block
+	});
+
+	$("#add-comm-03").click(function(){
+		$("#comm-03").toggleClass('comm-visible'); // Show add comment block
+	});
+
 	$(".sect-title").sticky({ topSpacing: 0, bottomSpacing: 840, center:true, className:"title-stick" }); // Sticker page title
 	$(".subnav").sticky({ topSpacing: 140, bottomSpacing: 400, center:true, className:"subnav-stick" }); // Sticker page subnav
 });
